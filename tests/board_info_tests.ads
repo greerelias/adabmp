4d778b149584ffdacf7f4ef7c80cbe55550dfd2e
Samuel Shumake <samshumake@bakufu.mynetworksettings.com>
2026-01-17 11:55:11 -0500

    add basic placeholders for get board info tests

---
+++ tests/board_info_tests.ads
@@ -0,0 +1,14 @@
+with AUnit.Test_Fixtures;
+
+package Board_Info_Tests is
+
+   type Test is new AUnit.Test_Fixtures.Test_Fixture with null record;
+
+   procedure Test_Board_Info_Success (T : in out Test);
+
+   -- TODO: Come up with test prototypes
+   --  procedure Test_Find_Device_Success (T : in out Test);
+   --  procedure Test_Find_Device_Not_Found (T : in out Test);
+   --  procedure Test_Find_Device_USB_Structure (T : in out Test);
+
+end Board_Info_Tests;
--- tests/test_runner_ts1.adb
+++ tests/test_runner_ts1.adb
@@ -3,6 +3,7 @@ with AUnit.Run;
 with AUnit.Test_Suites;
 with Connection_Tester_Tests;
 with Device_Discovery_Tests;
+with Board_Info_Tests;
 with Protocol_Tests;
 with AUnit.Test_Caller;
 
@@ -13,7 +14,10 @@ procedure Test_Runner_TS1 is
      AUnit.Test_Caller (Connection_Tester_Tests.Test);
    package Discovery_Caller is new
      AUnit.Test_Caller (Device_Discovery_Tests.Test);
-   package Protocol_Caller is new AUnit.Test_Caller (Protocol_Tests.Test);
+   package Protocol_Caller is new 
+     AUnit.Test_Caller (Protocol_Tests.Test);
+   package Board_Info_Caller is new
+     AUnit.Test_Caller (Board_Info_Tests.Test);
 
    function Suite return Access_Test_Suite is
       Ret : constant Access_Test_Suite := new Test_Suite;
@@ -85,6 +89,12 @@ procedure Test_Runner_TS1 is
         (Discovery_Caller.Create
            ("Test Find Device USB Structure",
             Device_Discovery_Tests.Test_Find_Device_USB_Structure'Access));
+
+      -- Get Board Info Tests
+      Ret.Add_Test
+         (Board_Info_Caller.Create
+            ("Test Get Board Info",
+            Board_Info_Tests.Test_Board_Info_Success'Access));
 
       return Ret;
    end Suite;
