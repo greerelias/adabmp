b853a7a94987b03cf9b6b7e2ebfedc7f77fa0c42
Samuel Shumake <shumakesam@gmail.com>
2026-02-19 18:31:08 -0500

    Revert "remove comm error board info test"
    This reverts commit 5dbd9b62017e96966598d06bc9af2f5f8b63cfe3.

--- tests/board_info_tests.ads
+++ tests/board_info_tests.ads
@@ -7,5 +7,6 @@ package Board_Info_Tests is
    procedure Test_Board_Info_Success (T : in out Test);
    procedure Test_Board_Info_Not_Found (T : in out Test);
    procedure Test_Board_Info_Format ( T : in out Test);
+   procedure Test_Board_Info_Comm_Error (T : in out Test);
 
 end Board_Info_Tests;
