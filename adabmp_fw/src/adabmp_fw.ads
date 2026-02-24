4fb2b418710810418c4734d9d3348d40cbf2078c
Greer Elias <greerelias@gmail.com>
2026-02-24 10:56:21 -0500

    removed state type

--- adabmp_fw/src/adabmp_fw.ads
+++ adabmp_fw/src/adabmp_fw.ads
@@ -8,19 +8,6 @@ with USB;
 
 package AdaBMP_FW is
 
-   type Programmer_State is
-     (Idle,
-      Testing_Connection,
-      Getting_Board_Info,
-      Configuring_Target,
-      Debugging_Target);
-
-   --  USB_Stack       : USB.Device.USB_Device_Stack (Max_Classes => 1);
-   --  Max_Packet_Size : constant := 64;
-   --  USB_Serial      :
-   --    aliased USB.Device.AdaBMP_Serial.Default_Serial_Class
-   --              (TX_Buffer_Size => 512, RX_Buffer_Size => 1024);
-   State  : Programmer_State := Idle;
    Info   : HAL.UInt32
    with Volatile, Export, Convention => C, External_Name => "db_info";
    USB_Tx : HAL.UInt8_Array (1 .. 64)
